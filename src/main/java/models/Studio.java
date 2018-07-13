d378c2e11b3d84c9b471a86ba07a123e00a75251
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-13 11:16:59 +0100

    studio model set up

--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -1,7 +1,10 @@
 package models;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "studio")
 public class Studio {
 
     private int id;
@@ -18,5 +21,41 @@ public class Studio {
         this.films = films;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "budget")
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    @Column(name = "films")
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
