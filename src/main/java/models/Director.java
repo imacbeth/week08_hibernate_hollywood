e42469aacb4bb1846e8244a1fc7d045980312d24
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-13 11:44:38 +0100

    director subclass set up

--- src/main/java/models/Director.java
+++ src/main/java/models/Director.java
@@ -1,4 +1,29 @@
 package models;
 
-public class Director {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "directors")
+public class Director extends Employee {
+
+    private List<Film> films;
+
+    public Director() {
+    }
+
+    public Director(String firstName, String lastName, double bankAccount) {
+        super(firstName, lastName, bankAccount);
+    }
+
+    @Column(name = "films")
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
