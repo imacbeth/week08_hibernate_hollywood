c818a36a43cee43931d5830cb557cdc66f445ab4
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-13 11:41:32 +0100

    employee abstract class and actor sub class set up

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -1,4 +1,29 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "actors")
 public class Actor extends Employee {
+
+    private List<Film> films;
+
+    public Actor() { }
+
+    public Actor(String firstName, String lastName, double bankAccount, List<Film> films) {
+        super(firstName, lastName, bankAccount);
+        this.films = films;
+    }
+
+    @Column(name = "films")
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
